1709f90dc7ffd31908f7bf17bc7ddbc1558666f4
Minfeng Lu <mlu@godaddy.com>
2017-11-29 10:30:32 -0800

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+var initialCats = [
+    {
+        clickCount : 0,
+        name : 'Tabby',
+        imgSrc : 'img/cat1.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568'
+    },
+    {
+        clickCount : 0,
+        name : 'Tiger',
+        imgSrc : 'img/cat2.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904'
+    },
+    {
+        clickCount : 0,
+        name : 'Scaredy',
+        imgSrc : 'img/cat3.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709'
+    },
+    {
+        clickCount : 0,
+        name : 'Shadow',
+        imgSrc : 'img/cat4.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559'
+    },
+    {
+        clickCount : 0,
+        name : 'Sleepy',
+        imgSrc : 'img/cat5.jpg',
+        imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288'
+    }
+]
